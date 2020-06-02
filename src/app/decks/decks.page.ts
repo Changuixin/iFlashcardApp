24bbc30ff2653869e812e3cf6eca5c84be68e60c
MarcoXin <591764955@qq.com>
2020-06-02 23:10:21 +0800

    留方法占坑

--- src/app/decks/decks.page.ts
+++ src/app/decks/decks.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-decks',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./decks.page.scss'],
 })
 export class DecksPage implements OnInit {
+  public COL_NUM: number = 3
+  public SCREEN_WIDTH: number = screen.width
+  public deckList: any = [
+    {
+      id: 11,
+      name: '四六级词汇',
+    },
+    {
+      id: 12,
+      name: '近现代史',
+    },
+  ]
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  // 翻转，如果用户已经收藏，则取消收藏；未收藏，则收藏牌组
+  flip(){
+
   }
 
+  // 判断用户是否收藏
+  isCollection(){
+    return false
+  }
 }
