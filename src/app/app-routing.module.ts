47585fd2b20525becf6da1a92e878e2282e818e2
MarcoXin <591764955@qq.com>
2020-06-02 15:58:49 +0800

    add login decks myDeck statistics router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,22 @@ const routes: Routes = [
     loadChildren: () =>
       import('./folder/folder.module').then((m) => m.FolderPageModule),
   },
+  {
+    path: 'v1/login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'v1/folder/decks',
+    loadChildren: () => import('./decks/decks.module').then( m => m.DecksPageModule)
+  },
+  {
+    path: 'v1/folder/my-deck',
+    loadChildren: () => import('./my-deck/my-deck.module').then( m => m.MyDeckPageModule)
+  },
+  {
+    path: 'v1/folder/statistics',
+    loadChildren: () => import('./statistics/statistics.module').then( m => m.StatisticsPageModule)
+  },
 ]
 
 @NgModule({
