2a1a6e1cd6e7274e5810a572c01fe99a3e56d68c
MarcoXin <591764955@qq.com>
2020-06-04 15:55:12 +0800

    依赖注入msgSerevice httpService以及router，编写登录方法

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+import { MessageService } from '../service/message.service'
+import { HttpService } from '../service/http.service'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  public username: string
+  public password: string
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public msgService: MessageService,
+    public httpService: HttpService
+  ) {}
 
-  ngOnInit() {
+  login() {
+    this.httpService.login(this.username, this.password).subscribe((res) => {
+      this.msgService.presentToast(res['meta']['msg'])
+      if (res['meta']['status'] == '200') {
+        localStorage['username'] = res['data']['username']
+        localStorage['userId'] = res['data']['userId']
+        this.router.navigate(['/v1/folder/my-deck'])
+      }
+    })
   }
-
 }
