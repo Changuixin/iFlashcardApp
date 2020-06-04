62fa1fb921673a265500ee78585569c8e0d95c90
MarcoXin <591764955@qq.com>
2020-06-04 16:48:26 +0800

    添加带输入框的alert

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -30,4 +30,43 @@ export class MessageService {
     })
     toast.present()
   }
+
+  /**
+   * 一个带输入框的alert
+   *
+   * @param header 标题
+   * @param placeholder 输入框的提示文字
+   * @param confirmCallback 确认按钮的回调函数
+   * @param cancelCallback 取消按钮的回调函数
+   */
+  async presentAlertPrompt(
+    header: string,
+    placeholder: string = '',
+    confirmCallback: any = () => {},
+    cancelCallback: any = () => {}
+  ) {
+    const alert = await this.alertController.create({
+      header: header,
+      inputs: [
+        {
+          name: 'data',
+          type: 'text',
+          placeholder: placeholder,
+        },
+      ],
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel',
+          handler: cancelCallback(),
+        },
+        {
+          text: '确定',
+          handler: (res) => confirmCallback(res),
+        },
+      ],
+    })
+
+    await alert.present()
+  }
 }
