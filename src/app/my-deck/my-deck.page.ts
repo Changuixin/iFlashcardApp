8b577e9de9362f1a11f0d0abd8a68f041a695aab
MarcoXin <591764955@qq.com>
2020-06-04 15:56:11 +0800

    完善登录部分方法

--- src/app/my-deck/my-deck.page.ts
+++ src/app/my-deck/my-deck.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { HttpService } from '../service/http.service'
+import { MessageService } from '../service/message.service'
+import { ActionSheetController } from '@ionic/angular'
 
 @Component({
   selector: 'app-my-deck',
@@ -11,48 +13,86 @@ export class MyDeckPage implements OnInit {
   public SCREEN_WIDTH: number = screen.width
   public deckList: any = [
     {
-      id: 1,
-      name: '软件需求分析',
+      deckId: 1,
+      deckName: '软件需求分析',
     },
     {
-      id: 2,
-      name: '线性代数',
+      deckId: 2,
+      deckName: '线性代数',
     },
     {
-      id: 3,
-      name: '近现代史',
-    },
-    {
-      id: 4,
-      name: '太极运动',
-    },
-    {
-      id: 5,
-      name: '高数',
-    },
-    {
-      id: 6,
-      name: '毛概',
-    },
-    {
-      id: 7,
-      name: '考研英语',
+      deckId: 3,
+      deckName: '近现代史',
     },
   ]
-  constructor(public httpService: HttpService) {}
+  constructor(
+    public httpService: HttpService,
+    public msgService: MessageService,
+    public actionSheetController: ActionSheetController
+  ) {}
 
   ngOnInit() {
-    this.httpService.getDeckList()
+    this.getDeckList()
+  }
+
+  // 获取牌组列表
+  getDeckList() {
+    this.httpService.getDeckList(localStorage['userId']).subscribe((res) => {
+      if (res['msg'] == '200') {
+        return (this.deckList = res['data'])
+      }
+      return (this.deckList = [])
+    })
+  }
+
+  async deckMenu(deckId: number) {
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [
+        {
+          text: '删除牌组',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => this.deleteDeck(deckId),
+        },
+        {
+          text: '修改牌组',
+          icon: 'pencil',
+          handler: () => {},
+        },
+      ],
+    })
+    await actionSheet.present()
   }
 
   // 学习牌组
   learnDeck() {
-    this.httpService.getCardList()
+    // this.httpService.getCardList()
   }
 
   // 添加牌组
   addDeck() {
-    this.httpService.addDeck()
-    this.httpService.getDeckList()
+    // this.httpService.addDeck()
+    // this.httpService.getDeckList()
+  }
+
+  // 删除牌组
+  deleteDeck(deckId: number) {
+    this.httpService.deleteDeck(deckId, localStorage['userId']).subscribe(
+      (res) => {
+        if (res['meta']['status'] == '200') {
+          this.msgService.presentToast('删除成功', 2000, 'success')
+          return this.getDeckList()
+        }
+        this.msgService.presentToast('删除失败', 2000, 'danger')
+      },
+      (err) => {
+        this.msgService.presentToast('删除失败', 2000, 'danger')
+      }
+    )
+  }
+
+  // 修改牌组名称
+  modifyDeck(deckId: number, deckName: string) {
+    this.httpService.modifyDeck(deckId, deckName).subscribe()
   }
 }
