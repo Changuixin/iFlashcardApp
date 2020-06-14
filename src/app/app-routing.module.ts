483ff1c4c355b3468e74f7255703b5a18b8d0637
MarcoXin <591764955@qq.com>
2020-06-14 17:43:17 +0800

    modify code block

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,35 +14,45 @@ const routes: Routes = [
   },
   {
     path: 'v1/login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'v1/folder/decks',
-    loadChildren: () => import('./decks/decks.module').then( m => m.DecksPageModule)
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'v1/folder/my-deck',
-    loadChildren: () => import('./my-deck/my-deck.module').then( m => m.MyDeckPageModule)
+    loadChildren: () =>
+      import('./my-deck/my-deck.module').then((m) => m.MyDeckPageModule),
+  },
+  {
+    path: 'v1/folder/decks',
+    loadChildren: () =>
+      import('./decks/decks.module').then((m) => m.DecksPageModule),
   },
   {
     path: 'v1/folder/statistics',
-    loadChildren: () => import('./statistics/statistics.module').then( m => m.StatisticsPageModule)
+    loadChildren: () =>
+      import('./statistics/statistics.module').then(
+        (m) => m.StatisticsPageModule
+      ),
   },
   {
-    path: 'v1/folder/help',
-    loadChildren: () => import('./help/help.module').then( m => m.HelpPageModule)
+    path: 'v1/folder/share',
+    loadChildren: () =>
+      import('./share/share.module').then((m) => m.SharePageModule),
   },
   {
     path: 'v1/folder/feedback',
-    loadChildren: () => import('./feedback/feedback.module').then( m => m.FeedbackPageModule)
+    loadChildren: () =>
+      import('./feedback/feedback.module').then((m) => m.FeedbackPageModule),
+  },
+  {
+    path: 'v1/folder/help',
+    loadChildren: () =>
+      import('./help/help.module').then((m) => m.HelpPageModule),
   },
   {
     path: 'v1/card/:deckId',
-    loadChildren: () => import('./card/card.module').then( m => m.CardPageModule)
-  },
-  {
-    path: 'share',
-    loadChildren: () => import('./share/share.module').then( m => m.SharePageModule)
+    loadChildren: () =>
+      import('./card/card.module').then((m) => m.CardPageModule),
   },
 ]
 
