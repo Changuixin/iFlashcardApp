b35d71cf46c9d6356c33acfc9bf75105960a381b
MarcoXin <591764955@qq.com>
2020-07-01 17:13:23 +0800

    完善feedback

--- src/app/feedback/feedback.page.ts
+++ src/app/feedback/feedback.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { SocialSharing } from '@ionic-native/social-sharing/ngx'
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.page.scss'],
 })
 export class FeedbackPage implements OnInit {
+  public subject: string
+  public body: string
 
-  constructor() { }
+  constructor(private socialSharing: SocialSharing) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  sendEmail() {
+    console.log(this.body);
+    
+    this.socialSharing
+      .shareViaEmail(this.body, this.subject, ['591764955@qq.com'])
+      .then(() => {
+        // Success!
+      })
+      .catch(() => {
+        // Error!
+      })
   }
-
 }
