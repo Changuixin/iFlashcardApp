7b1685c6c7894ac58bf70ad6ebcaab7c74d16f3c
MarcoXin <591764955@qq.com>
2020-07-01 23:27:34 +0800

    修改根目录重定向

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'v1/login',
+    redirectTo: 'v1/folder/my-deck',
     pathMatch: 'full',
   },
   {
