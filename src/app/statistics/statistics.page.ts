666f32245f5e6b360ac922c5b43e37c2adc98e34
MarcoXin <591764955@qq.com>
2020-07-01 16:07:53 +0800

    完善统计功能

--- src/app/statistics/statistics.page.ts
+++ src/app/statistics/statistics.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HttpService } from '../service/http.service'
+import { MessageService } from '../service/message.service'
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.page.scss'],
 })
 export class StatisticsPage implements OnInit {
+  public totalCardNumber: Number = 0
+  public deckList: any = []
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private msgService: MessageService
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.getDeckList()
   }
 
+  // 获取牌组列表
+  getDeckList() {
+    this.httpService.getDeckList(localStorage['userId']).subscribe((res) => {
+      console.log(res)
+
+      if (res['meta']['status'] == '200') {
+        this.deckList = res['data']
+        
+        this.getTotalCardNumber()
+      } else {
+        this.msgService.presentToast('获取牌组信息失败', 2000, 'danger')
+      }
+    })
+  }
+
+  getTotalCardNumber() {
+    setTimeout(() => {
+      this.totalCardNumber = 0
+      for (let index = 0; index < this.deckList.length; index++) {
+        const cardNumber = this.deckList[index].cardTotal
+
+        this.totalCardNumber += cardNumber
+      }
+    }, 0)
+  }
 }
